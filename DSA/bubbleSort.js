6d5b2ccbf4de10219ee7d41fe5a03ee4c69dc9e4
ratatatatcode <pogisijamesmagnaye@gmail.com>
2024-02-09 17:24:32 +0800

    Add new, basta new hahaha

---
+++ DSA/bubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = function(arr) {
+    const n = arr.length;
+    for(let i = 1; i < n; i++) {
+        for(let j = 0; j < n - i; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    let output = "";
+    for(let k = 0; k < n; k++) {
+        output += arr[k] + " ";
+    }
+
+    console.log(output);
+}
+
+let arr = [5,3,1,7,8,6]
+bubbleSort(arr);
